0633739cf58af1f1602ce7effbb8c83440036c5f
Lekkyjay <lekkyjayjay@yahoo.com>
2020-05-18 16:16:12 +0200

    Routes and basic controllers are working
    Created folders: routes and controllers
    Created file users.js in routes and controllers folder
    Installed dependency: express-promise-router
    Defined required routes and controllers
    Created MW route for users routes in app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from simple express server');
-});
+//Middlewares
+app.use(morgan('dev'));
+
+//Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+app.use('/users', require('./routes/users'));
+
 
 app.listen(port, function(){
     console.log("Server running on localhost:" + port);
