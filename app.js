bcaadc12db5782e7b3676d5f05e2f5f419564b72
Lekkyjay <lekkyjayjay@yahoo.com>
2020-05-18 21:33:48 +0200

    Installed mongoose and connected to mongoDB
    Installed and reqd dotenv to securely store all app credentials
    Created .env config file to hold app credentials

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+//CONNECT TO DB
+mongoose.connect(process.env.DB_CONNECTION, 
+  {useNewUrlParser: true,  useUnifiedTopology: true})
+  .then(() => { console.log('You are connected to MongoDB. Great!') })
+  .catch((err) => { console.log('Connection failed...' + err) });
 
 const app = express();
 
@@ -13,7 +19,8 @@ app.use(bodyParser.json());
 //Routes
 app.use('/users', require('./routes/users'));
 
-
+//Set listening port and start the server
+const port = process.env.PORT || 3000;
 app.listen(port, function(){
     console.log("Server running on localhost:" + port);
 });
